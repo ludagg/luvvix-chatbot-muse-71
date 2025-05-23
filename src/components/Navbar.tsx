26fc7ad1528266f0446bf99276e4117b4acfacf7
Ludovic Aggaï <sepogonangabang@gmail.com>
2025-05-23 14:40:08 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -171,8 +171,7 @@ const Navbar = () => {
 
           <div className="flex items-center gap-3">
             <WeatherWidget />
-            <ThemeToggle />
-            
+                        
             {user ? (
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>

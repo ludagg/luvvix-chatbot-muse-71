f639fc4452e16e5bbc3de48893d380b25f411510
Ludovic Aggaï <sepogonangabang@gmail.com>
2025-05-25 09:08:28 +0100

    Update TranslatePage.tsx

--- src/pages/TranslatePage.tsx
+++ src/pages/TranslatePage.tsx
@@ -20,7 +20,7 @@ const TranslatePage = () => {
 
       <div className="min-h-screen flex flex-col">
         <Navbar />
-        <div className="pt-45 flex-1">
+        <div className="pt-55 flex-1">
           <main className="flex-1">
             <LuvvixTranslate />
           </main>
